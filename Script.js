6244b0e6650f9d4543ce37f31e544695a5667d60
Mohak-Mittal <mohakmittal91@gmail.com>
2026-03-17 22:45:30 +0530

    Add project files.

---
+++ Script.js
@@ -0,0 +1,9 @@
+function openProjects() {
+    document.getElementById("projects").classList.add("active");
+    document.getElementById("main").classList.add("shrink");
+}
+
+function closeProjects() {
+    document.getElementById("projects").classList.remove("active");
+    document.getElementById("main").classList.remove("shrink");
+}
